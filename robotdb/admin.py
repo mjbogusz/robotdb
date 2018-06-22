6e9bce69b8d3436a609132383c0fa0e3f38169ba
mjbogusz <mjbogusz+github@gmail.com>
2018-06-22 23:21:31 +0200

    admin: added missing models

--- robotdb/admin.py
+++ robotdb/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Robot
-from .models import RobotImage
+from .models import Application, Article, Feature, Project, Robot, RobotImage
 
+admin.site.register(Application)
+admin.site.register(Article)
+admin.site.register(Feature)
+admin.site.register(Project)
 admin.site.register(Robot)
 admin.site.register(RobotImage)
