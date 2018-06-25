ae904b0924d742fb9b6e524368450856c5db1042
mjbogusz <mjbogusz+github@gmail.com>
2018-06-25 21:34:48 +0200

    admin: fixed robot/feature display

--- robotdb/admin.py
+++ robotdb/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Application, Article, Feature, Project, Robot, RobotImage
+from robotdb.models import Application, Article, Feature, Project, Robot, RobotFeature, RobotImage
+
+class RobotFeatureInline(admin.TabularInline):
+	model = RobotFeature
+	extra = 1
+
+class RobotAdmin(admin.ModelAdmin):
+	inlines = (RobotFeatureInline, )
+
+class FeatureAdmin(admin.ModelAdmin):
+	inlines = (RobotFeatureInline, )
+
+class RobotImageAdmin(admin.ModelAdmin):
+	exclude = ('height', 'width',)
 
 admin.site.register(Application)
 admin.site.register(Article)
-admin.site.register(Feature)
+admin.site.register(Feature, FeatureAdmin)
 admin.site.register(Project)
-admin.site.register(Robot)
-admin.site.register(RobotImage)
+admin.site.register(Robot, RobotAdmin)
+admin.site.register(RobotImage, RobotImageAdmin)
