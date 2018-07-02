112d0f2c93e60d698e5c5b15ca10d660e3e5a852
mjbogusz <mjbogusz+github@gmail.com>
2018-07-02 20:09:59 +0200

    renamed settings.py and added base filename to ignored - for each host its own

--- robotdb/_settings.py
+++ robotdb/_settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/robot_static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static'),
 	os.path.join(BASE_DIR, 'media'),
@@ -131,5 +131,5 @@ STATICFILES_FINDERS = [
 SASS_PROCESSOR_ROOT = 'static'
 
 # Media files
-MEDIA_URL = '/media/'
+MEDIA_URL = '/robot_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
