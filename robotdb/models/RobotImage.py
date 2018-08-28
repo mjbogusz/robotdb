d0b3d48e1b82ee8633ebc0eb9f3a0ad9fac5fbc2
mjbogusz <mjbogusz+github@gmail.com>
2018-08-28 16:24:31 +0200

    thumbnail size increased to 200x200

--- robotdb/models/RobotImage.py
+++ robotdb/models/RobotImage.py
@@ -9,7 +9,7 @@ from .utils import getRobotImagePath
 class RobotImage(models.Model):
 	robot = models.ForeignKey(Robot, on_delete = models.CASCADE)
 	image = models.ImageField(upload_to = getRobotImagePath, height_field = 'height', width_field = 'width', unique = True)
-	thumbnail = ImageSpecField(source = 'image', processors = [ResizeToFit(150, 150)], format = 'JPEG', options = {'quality': 90})
+	thumbnail = ImageSpecField(source = 'image', processors = [ResizeToFit(200, 200)], format = 'JPEG', options = {'quality': 90})
 	description = models.CharField('Image description', max_length = 200, blank = True)
 	height = models.PositiveIntegerField('Image height', blank = True)
 	width = models.PositiveIntegerField('Image width', blank = True)
